4eafd2510f7d1bf386c4f06441900d867addfafb
istikhar155 <istikhar@msn.com>
2026-01-11 12:54:34 -0500

    Update streamlit_app.py
    user -> to use

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if ingredients_list:
 # session = get_active_session()
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
+sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 # # Use an interactive slider to get user input
 # hifives_val = st.slider(
 #   "Number of high-fives in Q3",
